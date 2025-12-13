a9e2a1418d58652ae810eaaa15d27e3267be9bef
HadssanK <hassankhan847754@gmail.com>
2025-12-13 05:08:59 +0500

    updated

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,13 +1,13 @@
 
-import React, { Suspense } from "react";
+i
 import Checkout from "../src/Pages/Checkout"
 
 // export const dynamic = "force-dynamic";
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    // <Suspense fallback={<div>Loading...</div>}>
       
     <Checkout/>
-     </Suspense>
+    //  </Suspense>
   );
 }
